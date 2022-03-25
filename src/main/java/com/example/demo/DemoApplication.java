4371de39a3950bb04d68765ae6795fd299d0e13f
anderson.silva <anderson.silva@ip-192-168-0-11.sa-east-1.compute.internal>
2022-03-25 13:42:54 -0300

    feat!: remove print b

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		System.out.print("b");
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
