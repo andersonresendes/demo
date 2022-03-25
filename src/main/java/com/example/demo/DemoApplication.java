b82832558f271e422a2964f766c2deb2092c3461
anderson.silva <anderson.silva@ip-192-168-0-11.sa-east-1.compute.internal>
2022-03-25 13:40:55 -0300

    fix: remove print a

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		System.out.print("a");
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
