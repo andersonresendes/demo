fe76fed2051f30785b2692ecf1d3c9d24bba64b9
anderson.silva <anderson.silva@ip-192-168-0-11.sa-east-1.compute.internal>
2022-03-25 15:20:38 -0300

    feat: print a

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		System.out.print("sdf");
+		System.out.print("sd");
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
